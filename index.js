da513861bc245426c0fa80ded805377200cf47cd
mynulislam75 <mynulislam181120@gmail.com>
2021-12-12 02:01:16 +0600

    okkk

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port =process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cknzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// console.log(uri)
+// console.log(uri)feefefr
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
